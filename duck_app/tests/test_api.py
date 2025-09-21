8a0efdd59eb4eb2e6b76bd9ab1e825b6cd04289f
Tanveer Ahmad <tanveer.ahmadbackend@gmail.com>
2025-09-21 19:29:43 +0500

    feature complete

--- duck_app/tests/test_api.py
+++ duck_app/tests/test_api.py
@@ -15,3 +15,19 @@ def test_test_api(client):
     assert data["status"] == "success"
     assert data["message"] == "Test API is working!"
     assert data["environment"] == "staging"
+
+
+@pytest.mark.django_db
+def test_cricket_report_api(client):
+    """
+    Test that /api/test/ returns 200 and correct JSON response.
+    """
+    url = reverse('cricket-report')  # Uses the URL name from urls.py
+    response = client.get(url)
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert data["status"] == "success"
+    assert data["message"] == "Pak vs India match"
+    assert data["environment"] == "feature"
--- duck_app/urls.py
+++ duck_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import test_api
+from .views import test_api, cricket_report
 
 urlpatterns = [
     path('api/test/', test_api, name='test-api'),
+    path('api/cricket/', cricket_report, name='cricket-report'),
 ]
